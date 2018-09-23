38e5d8e63495619c2abd4a0112bccd0aed4a146c
Fran Jurinec <franjurinec@live.com>
2018-09-23 23:57:43 +0200

    Loading animation

--- app/src/main/java/hr/fer/android/qr/StartupActivity.java
+++ app/src/main/java/hr/fer/android/qr/StartupActivity.java
@@ -1,9 +1,42 @@
 package hr.fer.android.qr;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 
-public class StartupActivity extends Activity {
+import hr.fer.android.sglab.qr.R;
 
+public class StartupActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        RotateAnimation anim = new RotateAnimation(0.0f, 360.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setDuration(700);
+        findViewById(R.id.logo_circle1).startAnimation(anim);
+
+        RotateAnimation anim2 = new RotateAnimation(0.0f, -360.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        anim2.setInterpolator(new LinearInterpolator());
+        anim2.setRepeatCount(Animation.INFINITE);
+        anim2.setDuration(1500);
+        findViewById(R.id.logo_circle2).startAnimation(anim2);
+
+        RotateAnimation anim3 = new RotateAnimation(0.0f, 360.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        anim3.setInterpolator(new LinearInterpolator());
+        anim3.setRepeatCount(Animation.INFINITE);
+        anim3.setDuration(1100);
+        findViewById(R.id.logo_circle3).startAnimation(anim3);
+
+    }
 }
